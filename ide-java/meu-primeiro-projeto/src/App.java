283c5384731de81d4b292113f45bd3a8398f9e18
alex-sqls <alex-sql@outlook.com>
2025-02-17 17:56:45 -0300

    Meu primeiro projeto java

--- ide-java/meu-primeiro-projeto/src/App.java
+++ ide-java/meu-primeiro-projeto/src/App.java
@@ -1,7 +1,8 @@
 public class App {
+//outras IDE recomendadas: eclipse; intellij; netbeans
     public static void main(String[] args) {
         System.out.println("Ola mundo");
-        System.out.println("Que bom que soube disso");
+        System.out.println("Boas vindas turma DIO");
     }
 }
 
